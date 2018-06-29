e8a7d1aac9d9b0b0ef0e2184e4344563bc43604e
Peter Krenesky <pkrenesky@counsyl.com>
2018-06-28 21:41:30 -0700

    update setup, add meta, add entrypoint

--- setup.py
+++ setup.py
@@ -22,13 +22,19 @@ setup(
     maintainer='Peter Krenesky',
     maintainer_email='kreneskyp@gmail.com',
     description='Task tool with make-like features.',
+    keywords='tasks, shovel, power_shovel, rake, make',
     long_description=open('%s/README.md' % DIR, 'r').read(),
     url='https://github.com',
-    packages=find_packages(exclude=["*.tests",
-                                    "*.tests.*",
-                                    "tests.*",
-                                    "tests"]),
+    packages=['power_shovel'],
+    package_dir={'power_shovel': 'power_shovel'},
     include_package_data=True,
+    entry_points={'console_scripts': ['s2 = power_shovel:cli']},
     install_requires=requirements,
     zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ]
 )
