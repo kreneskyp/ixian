41cf9923ed2edfa7713fa23852512a3b3bb4231e
Peter Krenesky <pkrenesky@counsyl.com>
2019-11-03 20:39:32 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os
-from setuptools import find_packages, setup
 
-from pip.download import PipSession
-from pip.req import parse_requirements
+from pip._internal.download import PipSession
+from pip._internal.req import parse_requirements
+from setuptools import setup
 
 from power_shovel import VERSION
 
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-requirements_path = "%s/requirements.txt" % DIR
+requirements_path = f"{DIR}/requirements.txt"
 requirements = [
     str(ir.req) for ir in parse_requirements(requirements_path, session=PipSession())
 ]
+
 
 setup(
     name="power_shovel",
