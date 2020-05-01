3f37604bae42d51b16b44bb0de75140483eca0a8
Peter Krenesky <pkrenesky@counsyl.com>
2020-05-01 08:10:33 -0700

    update pip classifiers

--- setup.py
+++ setup.py
@@ -60,7 +60,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 3 - Alpha",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools ",
     ],
 )
