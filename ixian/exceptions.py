508e2bf56cece1f80fefe5eae2580422aa0c7035
Peter Krenesky <pkrenesky@counsyl.com>
2020-05-01 19:54:22 -0700

    linting

--- ixian/exceptions.py
+++ ixian/exceptions.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class AlreadyComplete(Exception):
     """
     Exception thrown when a Task executes but it's checks indicate complete.
--- ixian/utils/decorators.py
+++ ixian/utils/decorators.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class ClassPropertyDescriptor(object):
     def __init__(self, fget, fset=None):
         self.fget = fget
