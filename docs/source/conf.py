b370716b1210f5a349e48a362640703ba56b954f
Peter Krenesky <pkrenesky@counsyl.com>
2020-05-10 16:31:48 -0700

    add doc build to tox

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,9 +49,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# -- API Doc -----------------------------------------------------------------
-#apidoc_module_dir = "../../ixian"
-#apidoc_output_dir = "reference"
-#apidoc_excluded_paths = ["tests"]
-#apidoc_separate_modules = True
